3bde7837b2b7ee901fb60d8109a4de61733c222d
Yann Kamche <yannkamche@gmail.com>
2024-02-17 04:16:54 +0100

    add assets folder

---
+++ client/src/assets/index.js
@@ -0,0 +1,20 @@
+import lucydBLogo from './LucydBrand.svg'
+import facebookLogo from './facebook.svg'
+import women from './women.png'
+import background from './background.svg'
+import icon from './icon.svg'
+import googleLogo from './google.svg'
+import profile from './profile.svg'
+import lock from './lock.svg'
+import man from './man.png'
+export {
+    lucydBLogo,
+    facebookLogo,
+    women,
+    background,
+    icon,
+    googleLogo,
+    profile,
+    lock,
+    man
+}
