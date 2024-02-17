16c8e6f2a8937f229df919ffe4fbc3a626184542
Yann Kamche <yannkamche@gmail.com>
2024-02-17 17:37:50 +0100

    Adjusted the margin and added a shadow

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -54,83 +54,83 @@ const SignUp = () => {
           
 }
   return (
-     <div className='w-full h-screen flex justify-center items-center'>
-     <div className='flex justify-center items-center flex-row w-full'>
+     <div className='w-full h-screen flex justify-center items-center p-4'>
+      <div className='flex justify-center items-center flex-row w-full max-w-5xl shadow-2xl rounded-3xl'>
 
-      <div className='flex flex-col items-center justify-center gap-4 max-lg:w-full lg:w-1/2 '>
-        {/* Logo */}
-        <img src={lucydBLogo} alt="logo" className='mb-3'/>
+        <div className='flex flex-col items-center justify-center gap-4 max-lg:w-full lg:w-1/2 '>
+          {/* Logo */}
+          <img src={lucydBLogo} alt="logo" className='mt-5 mb-3'/>
 
-        {/* titles */}
-        <div>
-            <p className='text-center text-3xl font-poppins font-bold'>LOGIN</p>
-            <p className='text-center font-poppins font-normal text-lg'>Welcome Back!</p>
-        </div>
+          {/* titles */}
+          <div>
+              <p className='text-center text-3xl font-poppins font-bold'>LOGIN</p>
+              <p className='text-center font-poppins font-normal text-lg'>Welcome Back!</p>
+          </div>
 
-        {/* Form */}
-         <div className='max-md:w-4/5 md:w-1/2'>
-          <form onSubmit={handleSubmit} action="" className='mt-3 flex flex-col gap-2 w-full'>
-            <div className='w-full relative'>
-              <div className='absolute top-3 left-3'>
-                <img src={profile} alt="profile" />
+          {/* Form */}
+          <div className='max-md:w-4/5 md:w-1/2'>
+            <form onSubmit={handleSubmit} action="" className='mt-3 flex flex-col gap-2 w-full'>
+              <div className='w-full relative'>
+                <div className='absolute top-3 left-3'>
+                  <img src={profile} alt="profile" />
+                </div>
+                <input 
+                className='input'
+                type="text"
+                name='username'
+                id='username'
+                placeholder='Username' 
+                onChange={handleChange}                                   
+                />
+                
               </div>
-              <input 
-              className='input'
-              type="text"
-              name='username'
-              id='username'
-              placeholder='Username' 
-              onChange={handleChange}                                   
-              />
-              
+
+              <div className='relative w-full'>
+                <div className='absolute top-3 left-3'>
+                  <img src={profile} alt="profile" />
+                </div>
+                <input 
+                className='input'
+                type="email"
+                name='email'
+                id='email'
+                placeholder='Email' 
+                onChange={handleChange}
+                />
+              </div>
+            </form>
+          </div>
+
+          {/* Buttons */}
+          <div className='flex flex-col gap-2 max-md:w-4/5 md:w-1/2 mt-4'>
+            <button disabled={loading} className='login-button max-md:w-1/2 md:w-1/2 cursor-pointer'>
+              {loading ? 'Loading...': 'Sign In'}
+            </button>
+          
+            <button className='button flex justify-center items-center gap-2 disabled:opacity-80'><img src={googleLogo} alt="google" /> Log in with<span className='font-bold'>Google</span></button>
+            <button className='button flex justify-center items-center gap-2'><img src={facebookLogo} alt="facebook" />Log in with <span className='font-bold'>Facebook</span></button>
+          </div>
+
+          <p className='flex text-sm gap-8'>Dont have an account? <Link to='/sign-up'><span className='font-bold underline'>Sign Up</span></Link></p>
+          <p className='text-red-700 mt-5'>{error && 'Something went wrong'}</p>
+        </div>
+        
+        <div className='hidden lg:flex justify-center items-center w-[500px] h-[570px] -mr-2 relative rounded-br-3xl rounded-tr-3xl' style={{ backgroundImage: `url(${background})`}}>
+          <div className='relative w-[60%] h-[60%] border-white border bg-color border-solid rounded-[30px]' style={{backgroundColor: '#9889F5'}}>
+            <div className='absolute bottom-0 translate-x-10'>
+              <img src={women} alt="man" width={600} />
             </div>
 
-            <div className='relative w-full'>
-              <div className='absolute top-3 left-3'>
-                <img src={profile} alt="profile" />
-              </div>
-              <input 
-              className='input'
-              type="email"
-              name='email'
-              id='email'
-              placeholder='Email' 
-              onChange={handleChange}
-              />
+            <div className='absolute top-3 left-3 w-1/2'>
+              <p className='font-poppins text-xl text-white font-bold'>Very good works are waiting for you. Sign Up Now!!!</p>
             </div>
-          </form>
-        </div>
 
-        {/* Buttons */}
-        <div className='flex flex-col gap-2 max-md:w-4/5 md:w-1/2 mt-4'>
-          <button disabled={loading} className='login-button max-md:w-1/2 md:w-1/2 cursor-pointer'>
-            {loading ? 'Loading...': 'Sign In'}
-          </button>
-         
-          <button className='button flex justify-center items-center gap-2 disabled:opacity-80'><img src={googleLogo} alt="google" /> Log in with<span className='font-bold'>Google</span></button>
-          <button className='button flex justify-center items-center gap-2'><img src={facebookLogo} alt="facebook" />Log in with <span className='font-bold'>Facebook</span></button>
-        </div>
-
-        <p className='flex text-sm gap-8'>Dont have an account? <Link to='/sign-up'><span className='font-bold underline'>Sign Up</span></Link></p>
-        <p className='text-red-700 mt-5'>{error && 'Something went wrong'}</p>
-      </div>
-       
-      <div className='hidden lg:flex justify-center items-center w-[500px] h-[550px] mr-3 relative' style={{ backgroundImage: `url(${background})`}}>
-        <div className='relative w-[60%] h-[60%] border-white border bg-color border-solid rounded-[30px]' style={{backgroundColor: '#9889F5'}}>
-          <div className='absolute bottom-0 translate-x-10'>
-            <img src={women} alt="man" width={600} />
-          </div>
-
-          <div className='absolute top-3 left-3 w-1/2'>
-            <p className='font-poppins text-xl text-white font-bold'>Very good works are waiting for you. Sign Up Now!!!</p>
-          </div>
-
-          <div className='absolute -left-5 bottom-12'>
-            <img src={icon} alt="icon" width={60}/>
+            <div className='absolute -left-5 bottom-12'>
+              <img src={icon} alt="icon" width={60}/>
+            </div>
           </div>
         </div>
       </div>
-     </div>
     </div>
   )
 }
