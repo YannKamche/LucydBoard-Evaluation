206af5e64e967923d237fae3de782b8cd31760ed
Yann Kamche <yannkamche@gmail.com>
2024-02-22 09:49:20 +0100

    Setting up material Ui for light and dark Theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import { WelcomeLogin, AdminDashboard, SignIn, SignUp, UserDashboard } from './pages'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from '../theme'
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
 
 const App = () => {
+  //grab the state we created
+  const mode = useSelector(state => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); //provide light and dark mode
+
+
   return (
-    <BrowserRouter>
+    //Setting up material UI
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
       <Routes>
         <Route path="/" element={<WelcomeLogin />} />
         <Route path="/sign-in" element={<SignIn />} />
@@ -12,6 +24,8 @@ const App = () => {
         <Route path="user-dashboard" element={<UserDashboard />} />
       </Routes>
     </BrowserRouter>
+    <CssBaseline />
+    </ThemeProvider>
   )
 }
 
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,25 +1,35 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
 import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; //save data inside the local storage of the browser
+import storage from 'redux-persist/lib/storage'; // save data inside the local storage of the browser
 
-const rootReducer = combineReducers({ user: userReducer });
+// Import the global reducer
+import globalReducer from './dashboard/index.js';
 
-//Define persist config
+// Combine all reducers
+const rootReducer = combineReducers({
+  user: userReducer,
+  global: globalReducer, // Add the global reducer here
+});
+
+// Define persist config
 const persistConfig = {
   key: 'root',
   version: 1,
   storage,
 };
 
+// Wrap the rootReducer with persistReducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// Configure the Redux store
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: persistedReducer, // Pass the persistedReducer directly
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
 
+// Create the persistor
 export const persistor = persistStore(store);
